70bc58414c4e40a363eecf41c23e35743fe5991d
glenn222 <glendon.ngo@gmail.com>
2019-05-10 00:37:11 -0400

    Ionic List Dividers
    Add <ion-item-dividers> into our teams page.
    Used lodash to split the huge teams list and organize them by division containing a name and their respective group of teams.

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TeamHomePage } from '../team-home/team-home';
 import { EliteScheduleApi } from '../../providers/elite-schedule-api/elite-schedule-api';
-
+import * as _ from 'lodash';
+import { callLifecycleHooksChildrenFirst } from '@angular/core/src/view/provider';
 
 
 @Component({
@@ -12,8 +13,11 @@ import { EliteScheduleApi } from '../../providers/elite-schedule-api/elite-sched
 export class TeamsPage {
 
   public teams = [];
+  private allTeams: any;
+  private allTeamDivisions: any;
 
   constructor(
+    private loadingController: LoadingController,
     public navCtrl: NavController,
     public navParams: NavParams,
     private eliteScheduleApi: EliteScheduleApi) {
@@ -22,9 +26,28 @@ export class TeamsPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamsPage');
     let selectedTourney = this.navParams.data;
-    this.eliteScheduleApi.getTournamentData(selectedTourney.id).subscribe(
-      data => {
-        this.teams = data.teams;
+
+    let loader = this.loadingController.create({
+      content: 'Getting data..'
+    });
+
+    loader.present().then(() => {
+      this.eliteScheduleApi.getTournamentData(selectedTourney.id).subscribe(
+        data => {
+          this.allTeams = data.teams;
+  
+          this.allTeamDivisions = 
+            _.chain(data.teams)
+              .groupBy('division')
+              .toPairs()
+              .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+              .value();
+          
+          this.teams = this.allTeamDivisions;
+          console.log('Division teams ', this.teams);
+
+          loader.dismiss();
+        });
       });
   }
 
